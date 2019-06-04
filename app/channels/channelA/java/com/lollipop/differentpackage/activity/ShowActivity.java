59cc4cfbbf2020bd4d6db7f3cbb1dcbb9e44ec99
lollipop <976731597@qq.com>
2019-06-04 18:57:11 +0800

    Initial commit

---
+++ app/channels/channelA/java/com/lollipop/differentpackage/activity/ShowActivity.java
@@ -0,0 +1,15 @@
+package com.lollipop.differentpackage.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lollipop.differentpackage.R;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show2);
+    }
+}
---
+++ app/src/main/java/com/lollipop/differentpackage/MainActivity.java
@@ -0,0 +1,27 @@
+package com.lollipop.differentpackage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lollipop.differentpackage.activity.ShowActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvJump = findViewById(R.id.tv_jump);
+        tvJump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
